fc08936f8f60466427b37d798e80c7bd1aa60eb6
vunguyen <nguyenvanvu.a5.hongha@gmail.com>
2023-11-06 21:00:55 +0700

    thêm activity_welcome thêm animation welcome thêm color màu xanh thêm code fullscreen implement gradle

--- app/src/main/java/com/example/plantshop/activity_welcome.java
+++ app/src/main/java/com/example/plantshop/activity_welcome.java
@@ -3,18 +3,12 @@ package com.example.plantshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-
-public class MainActivity extends AppCompatActivity {
-
-
+public class activity_welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_welcome);
     }
 }
