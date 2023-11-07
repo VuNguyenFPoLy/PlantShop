7dde79698dfe3f8d4fcb3146784bacc64255a2cd
DinhNhanFpoly <hailuazzz2004@gmail.com>
2023-11-07 14:39:23 +0700

    push code đăng nhập, đăng ký

---
+++ app/src/main/java/com/example/plantshop/activity_DangKy.java
@@ -0,0 +1,14 @@
+package com.example.plantshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_DangKy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ky);
+    }
+}
