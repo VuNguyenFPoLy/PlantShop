d3efc5bc776cc1added263d95b1a807d25a95e0f
vunguyen <nguyenvanvu.a5.hongha@gmail.com>
2023-11-14 20:01:54 +0700

    add logout

--- app/src/main/java/com/example/plantshop/fragment/Fragment_Profile.java
+++ app/src/main/java/com/example/plantshop/fragment/Fragment_Profile.java
@@ -1,5 +1,6 @@
 package com.example.plantshop.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.plantshop.R;
+import com.example.plantshop.activity.Activity_DangNhap;
 
 public class Fragment_Profile extends Fragment {
 
@@ -23,6 +25,21 @@ public class Fragment_Profile extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragnment_profile, container, false);
+
+        img_Avatar = view.findViewById(R.id.img_Avatar);
+        tv_FullName = view.findViewById(R.id.tv_FullName);
+        tv_EmailUser = view.findViewById(R.id.tv_EmailUser);
+        tv_EditInformation = view.findViewById(R.id.tv_EditInformation);
+        tv_HandbookPlant = view.findViewById(R.id.tv_HandbookPlant);
+        tv_History = view.findViewById(R.id.tv_History);
+        tv_Help = view.findViewById(R.id.tv_Help);
+        tv_ChangePass = view.findViewById(R.id.tv_ChangePass);
+        tv_LogOut = view.findViewById(R.id.tv_LogOut);
+
+        tv_LogOut.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), Activity_DangNhap.class);
+            startActivity(intent);
+        });
         return view;
     }
 }
